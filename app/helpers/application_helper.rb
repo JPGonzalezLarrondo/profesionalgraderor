e579e8a192013200b4d5105661dab8f7b45a9906
Juan Pablo Gonzalez <jp.gonzalezlarrondo@gmail.com>
2019-01-11 11:36:45 -0300

    add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+# Returns the full title on a per-page basis.
+	def full_title(page_title = '')
+# me está buscadno dentro del page_title, osea, lo que está dentro de <title></title>
+	base_title = "Ruby on Rails Tutorial Sample App"
+		if page_title.empty?
+		base_title
+		else
+		page_title + " | " + base_title
+		end
+	end
 end
